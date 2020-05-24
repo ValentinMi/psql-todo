7fc80f121b09a993ed045d61a348456415608049
Valentin Misiaszek <mowerv6@gmail.com>
2020-05-25 01:00:27 +0200

    feat(todos): handle genre in todos routes

--- models/todo.model.js
+++ models/todo.model.js
@@ -2,15 +2,17 @@ const moment = require("moment");
 const Joi = require("@hapi/joi");
 
 class Todo {
-  constructor(description) {
+  constructor(description, genre_id) {
     this.description = description;
     this.creationDate = moment();
+    this.genre_id = genre_id;
   }
 }
 
 const validateTodo = todo => {
   const schema = Joi.object({
-    description: Joi.string().min(1).max(255).required()
+    description: Joi.string().min(1).max(255).required(),
+    genre_id: Joi.number().integer().min(0).required()
   });
 
   return schema.validate(todo);
--- routes/todos.routes.js
+++ routes/todos.routes.js
@@ -22,12 +22,12 @@ router.post("/", async (req, res) => {
     const { error } = validate(req.body);
     if (error) res.status(500).send("Bad request");
 
-    const newTodo = new Todo(req.body.description);
+    const newTodo = new Todo(req.body.description, req.body.genre_id);
 
     // Save in database
     await pool.query(
-      "INSERT INTO todos (description, creationDate) VALUES($1, $2)",
-      [newTodo.description, newTodo.creationDate]
+      "INSERT INTO todos (description, creationDate, genre_id) VALUES($1, $2, $3)",
+      [newTodo.description, newTodo.creationDate, newTodo.genre_id]
     );
 
     res.send(newTodo);
@@ -40,7 +40,7 @@ router.post("/", async (req, res) => {
 router.put("/:id", async (req, res) => {
   try {
     // Destructure
-    const { description } = req.body;
+    const { description, genre_id } = req.body;
     const { id } = req.params;
 
     // Validate req body
@@ -55,11 +55,11 @@ router.put("/:id", async (req, res) => {
     if (!todo) res.status(404).send("Todo not found");
 
     // Update todo
-    const updatedTodo = { ...todo, description };
-    await pool.query("UPDATE todos SET description = $1 WHERE todo_id = $2", [
-      updatedTodo.description,
-      id
-    ]);
+    const updatedTodo = { ...todo, description, genre_id };
+    await pool.query(
+      "UPDATE todos SET (description, genre_id) VALUES($1, $2) WHERE todo_id = $3",
+      [updatedTodo.description, updatedTodo.genre_id, id]
+    );
 
     res.send(updatedTodo);
   } catch (error) {
